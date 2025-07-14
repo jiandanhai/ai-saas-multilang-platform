da63d24606dfbb9f88dbf83e18491fd5a2fa41f7
jiandanhai <jiandanhai@outlook.com>
2025-07-14 08:20:18 +0800

    更改

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,14 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:password@localhost:5432/dbname"
-    SECRET_KEY: str = "your_secret_key"
-    DEEP_L_API_KEY: str = "your_deepL_api_key"
+    SECRET_KEY: str = ""
+    DEEP_L_API_KEY: str = ""
+    DATABASE_URL: str = ""
+    CELERY_BROKER_URL: str = ""
+    BAIDU_APP_ID: str = ""
+    BAIDU_API_KEY: str = ""
+    BAIDU_SECRET_KEY: str = ""
+
     #在 config.py 里自动读取.env
     class Config:
         env_file = ".env"
